90a59c5002871edf18fcf913438be8ac665e792d
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2017-07-19 01:04:17 +0200

    [src] Update

--- src/invariant/definition/netcdf/nodecurrent3d.cpp
+++ src/invariant/definition/netcdf/nodecurrent3d.cpp
@@ -74,8 +74,6 @@ const ibex::IntervalVector NodeCurrent3D::eval(const IntervalVector& position){
 void NodeCurrent3D::fill_leafs(const vector<vector<vector<short>>> &raw_u, const vector<vector<vector<short>>> &raw_v, const float& scale_factor, const short& fill_value){
 
     int nb_node=m_leaf_list.size();
-    size_t dim = m_position.size();
-
     int d_max[3] = {(int)(raw_u.size())-1, (int)(raw_u[0].size())-1, (int)(raw_u[0][0].size())-1};
 
     vector<double> grid_size = m_previmer->get_grid_size();
@@ -85,35 +83,63 @@ void NodeCurrent3D::fill_leafs(const vector<vector<vector<short>>> &raw_u, const
         NodeCurrent3D *nc = m_leaf_list[id];
 
         /// **** Interpolation of vector field ****
-        vector<vector<int>> tab_point;
+        vector<vector<int>> tab_point_u, tab_point_v;
 
         for(int dim = 0; dim<3; dim++){
-            vector<int> pt;
-            double center = nc->get_position()[dim].mid() / grid_size[dim]; // Back to the grid coord
+            vector<int> pt_u, pt_v;
+            Interval center_u = nc->get_position()[dim].mid() / grid_size[dim]; // Back to the grid coord
+            Interval center_v;
+            if(dim > 0)
+                center_v = (nc->get_position()[dim].mid()+0.5*grid_size[dim]) / grid_size[dim]; // Back to the grid coord
+            else
+                center_v = nc->get_position()[dim].mid() / grid_size[dim]; // Back to the grid coord
+
             // Cross pattern
-            for(int i=std::max(0, std::min((int)floor(center), d_max[dim]));
-                i<std::max(0, std::min((int)ceil(center), d_max[dim]));
+            for(int i=std::max(0, std::min((int)floor(center_u.lb()), d_max[dim]));
+                i<std::max(0, std::min((int)ceil(center_u.ub()), d_max[dim]));
                 i++){
-                pt.push_back(i);
+                pt_u.push_back(i);
             }
-            tab_point.push_back(pt);
+            for(int i=std::max(0, std::min((int)floor(center_v.lb()), d_max[dim]));
+                i<std::max(0, std::min((int)ceil(center_v.ub()), d_max[dim]));
+                i++){
+                pt_v.push_back(i);
+            }
+            tab_point_u.push_back(pt_u);
+            tab_point_v.push_back(pt_v);
         }
 
-        IntervalVector vector_field(dim, Interval::EMPTY_SET); // 3 Dimensions (dt, U, V)
+        IntervalVector vector_field(3, Interval::EMPTY_SET); // 3 Dimensions (dt, U, V)
+        // T
+        vector_field[0] = Interval(1.0);
         bool no_value = false;
-        // U & V
-        for(size_t t_id=0; t_id<tab_point[0].size(); t_id++){
-            for(size_t u_id=0; u_id<tab_point[0].size(); u_id++){
-                for(size_t v_id=0; v_id<tab_point[0].size(); v_id++){
-                    size_t t_coord = tab_point[0][t_id];
-                    size_t i_coord = tab_point[1][u_id];
-                    size_t j_coord = tab_point[2][v_id];
+        // U
+        for(size_t t_id=0; t_id<tab_point_u[0].size(); t_id++){
+            for(size_t u_id=0; u_id<tab_point_u[0].size(); u_id++){
+                for(size_t v_id=0; v_id<tab_point_u[0].size(); v_id++){
+                    size_t t_coord = tab_point_u[0][t_id];
+                    size_t i_coord = tab_point_u[1][u_id];
+                    size_t j_coord = tab_point_u[2][v_id];
 
                     short vec_u = raw_u[t_coord][i_coord][j_coord];
-                    short vec_v = raw_v[t_coord][i_coord][j_coord];
-                    if(vec_u!=fill_value && vec_v!=fill_value){
-                        vector_field[0] |= Interval(1.0);
+                    if(vec_u!=fill_value){
                         vector_field[1] |= Interval(vec_u*scale_factor);
+                    }
+                    else
+                        no_value = true;
+                }
+            }
+        }
+        // V
+        for(size_t t_id=0; t_id<tab_point_v[0].size(); t_id++){
+            for(size_t u_id=0; u_id<tab_point_v[0].size(); u_id++){
+                for(size_t v_id=0; v_id<tab_point_v[0].size(); v_id++){
+                    size_t t_coord = tab_point_v[0][t_id];
+                    size_t i_coord = tab_point_v[1][u_id];
+                    size_t j_coord = tab_point_v[2][v_id];
+
+                    short vec_v = raw_v[t_coord][i_coord][j_coord];
+                    if(vec_v!=fill_value){
                         vector_field[2] |= Interval(vec_v*scale_factor);
                     }
                     else
