0d15cc86d6079abf24306ed8651bdc2497554109
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2017-08-11 22:43:07 +0200

    [src] Add example (Pieter Collins)

---
+++ examples/cpp/trajectories/2D/02_collins_trajectory/main.cpp
@@ -0,0 +1,89 @@
+#include "graph.h"
+#include "domain.h"
+#include "dynamics_function.h"
+#include "maze.h"
+#include "vibes_graph.h"
+
+#include <iostream>
+#include "vibes/vibes.h"
+#include <cstring>
+#include <omp.h>
+
+using namespace std;
+using namespace ibex;
+using namespace invariant;
+
+int main(int argc, char *argv[])
+{
+    int iterations = 9;
+    ibex::Variable x1, x2;
+
+    IntervalVector space(2);
+    space[0] = Interval(-5,5);
+    space[1] = Interval(-3.5,10);
+    Graph graph(space);
+
+    // ****** Dynamics *******
+    ibex::Function f(x1, x2, Return(2*x1-x1*x2,2*pow(x1,2)-x2));
+    vector<Function *> f_list;
+    f_list.push_back(&f);
+    Dynamics_Function dyn(f_list);
+
+    // ****** Domain & Maze *******
+    invariant::Domain dom_A(&graph, LINK_TO_INITIAL_CONDITION);
+    dom_A.set_border_path_in(false);
+    dom_A.set_border_path_out(false);
+
+    Function f_sep_A(x1, x2, pow(x1, 2)+pow(x2+2, 2)-1);
+    SepFwdBwd s_A(f_sep_A, LEQ); // LT, LEQ, EQ, GEQ, GT)
+    dom_A.set_sep(&s_A);
+
+    Maze maze_A(&dom_A, &dyn, MAZE_FWD, MAZE_PROPAGATOR);
+
+    invariant::Domain dom_B(&graph, LINK_TO_INITIAL_CONDITION);
+    dom_B.set_border_path_in(false);
+    dom_B.set_border_path_out(false);
+
+    Function f_sep_B(x1, x2, pow(x1, 2)+pow(x2-1, 2)-9.0/100.0);
+    SepFwdBwd s_B(f_sep_B, LEQ); // LT, LEQ, EQ, GEQ, GT)
+    dom_B.set_sep(&s_B);
+
+    Maze maze_B(&dom_B, &dyn, MAZE_FWD, MAZE_PROPAGATOR);
+
+    dom_B.add_maze(&maze_A);
+    dom_A.add_maze(&maze_B);
+
+    double time_start = omp_get_wtime();
+    maze_A.init(); // To set first pave to be in
+    maze_B.init(); // To set first pave to be in
+    for(int i=0; i<iterations; i++){
+        cout << i << "/" << iterations-1 << endl;
+        graph.bisect();
+        maze_A.contract();
+        maze_B.contract();
+
+        maze_A.contract_inter();
+        maze_B.contract_inter();
+    }
+    cout << "TIME = " << omp_get_wtime() - time_start << endl;
+
+    cout << graph << endl;
+
+    Vibes_Graph v_graphA("graph_A", &graph, &maze_A);
+    v_graphA.setProperties(0, 0, 512, 512);
+    v_graphA.show();
+    vibes::drawCircle(0.0, -2.0, 1.0, "r[]");
+
+    Vibes_Graph v_graphB("graph_B", &graph, &maze_B);
+    v_graphB.setProperties(0, 0, 512, 512);
+    v_graphB.show();
+    vibes::drawCircle(0.0, 1.0, 9.0/100.0, "r[]");
+
+    vibes::endDrawing();
+
+//    IntervalVector position_info(2);
+//    position_info[0] = Interval(-2);
+//    position_info[1] = Interval(4);
+//    v_graph.get_room_info(&maze, position_info);
+
+}
