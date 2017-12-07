2e576d4ef22a10cc0c7f5f938d9bbb0b29d12649
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2017-12-08 00:58:21 +0100

    [src] Update

--- src/invariant/maze/door.h
+++ src/invariant/maze/door.h
@@ -262,8 +262,8 @@ protected:
 
     std::vector<bool>    m_possible_out;
     std::vector<bool>    m_possible_in;
-    bool                 m_possible_in_union;
-    bool                 m_possible_out_union;
+    bool                 m_possible_in_union = false;
+    bool                 m_possible_out_union = false;
     std::vector<bool>    m_collinear_vector_field;
     std::vector<std::vector<bool>> m_zeros_in_vector_fields;
 };
