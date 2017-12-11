1354fa4f5a9bad6ed44586748440b798d5c2b1dd
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2017-12-12 00:28:28 +0100

    [CMake] Update

--- cmake/build_VTK.sh
+++ cmake/build_VTK.sh
@@ -6,7 +6,7 @@
 
 #set -x # debugging
 
-if [ ! -e "$HOME/vtk/bin/vtkH5detect" ]; then
+if [ ! -e "$HOME/vtk/lib/libvtkCommonCore-8.0.so" ]; then
   echo 'Installing VTK-lib...';
   # git clone https://gitlab.kitware.com/vtk/vtk
   # cd vtk
