cc36c4243f6d6d6013b1cc39817085c89464b2ff
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-11-23 19:16:41 +0100

    [src] Update

--- examples/cpp/basin/12_zang_stay_bassin/main.cpp
+++ examples/cpp/basin/12_zang_stay_bassin/main.cpp
@@ -51,66 +51,66 @@ int main(int argc, char *argv[]){
     dom_inner.set_sep_input(&sep_dom_input);
 
     // Outer Basin
-    invariant::Domain<> dom_outer_basin(&subpaving, FULL_WALL);
-    dom_outer_basin.set_border_path_in(false);
-    dom_outer_basin.set_border_path_out(false);
+//    invariant::Domain<> dom_outer_basin(&subpaving, FULL_WALL);
+//    dom_outer_basin.set_border_path_in(false);
+//    dom_outer_basin.set_border_path_out(false);
 
-    invariant::Domain<> dom_inner_basin(&subpaving, FULL_DOOR);
-    dom_inner_basin.set_border_path_in(true);
-    dom_inner_basin.set_border_path_out(true);
+//    invariant::Domain<> dom_inner_basin(&subpaving, FULL_DOOR);
+//    dom_inner_basin.set_border_path_in(true);
+//    dom_inner_basin.set_border_path_out(true);
 
     /// ************************
     ///         Dynamics
     /// ************************
     ibex::Function f(x1, x2, Return(x2,
                                     (8.0/25.0*pow(x1,5)-4.0/3.0*pow(x1,3)+4.0/5.0*x1)));
-    ibex::Function f2(x1, x2, Return(x2,
-                                    (8.0/25.0*pow(x1,5)-4.0/3.0*pow(x1,3)+4.0/5.0*x1)));
+//    ibex::Function f2(x1, x2, Return(x2,
+//                                    (8.0/25.0*pow(x1,5)-4.0/3.0*pow(x1,3)+4.0/5.0*x1)));
     DynamicsFunction dyn(&f, FWD); // Duplicate because of simultaneous access of f (semaphore on DynamicsFunction)
-    DynamicsFunction dyn_basin(&f2, FWD);
+//    DynamicsFunction dyn_basin(&f2, FWD);
 
     // ******* Maze ********* //
     invariant::Maze<> maze_outer(&dom_outer, &dyn);
     invariant::Maze<> maze_inner(&dom_inner, &dyn);
 
-    invariant::Maze<> maze_outer_basin(&dom_outer_basin, &dyn_basin);
-    invariant::Maze<> maze_inner_basin(&dom_inner_basin, &dyn_basin);
+//    invariant::Maze<> maze_outer_basin(&dom_outer_basin, &dyn_basin);
+//    invariant::Maze<> maze_inner_basin(&dom_inner_basin, &dyn_basin);
 
-    invariant::BooleanTreeUnionIBEX outer_graph(&maze_outer, &maze_outer_basin);
-    invariant::BooleanTreeUnionIBEX inner_graph(&maze_inner, &maze_inner_basin);
-    invariant::BooleanTreeInterIBEX bisect_graph(&outer_graph, &inner_graph);
-    subpaving.set_bisection_tree(&bisect_graph);
+//    invariant::BooleanTreeUnionIBEX outer_graph(&maze_outer, &maze_outer_basin);
+//    invariant::BooleanTreeUnionIBEX inner_graph(&maze_inner, &maze_inner_basin);
+//    invariant::BooleanTreeInterIBEX bisect_graph(&outer_graph, &inner_graph);
+//    subpaving.set_bisection_tree(&bisect_graph);
 
-    dom_inner_basin.add_maze_initialization_inter(&maze_inner);
-    dom_outer_basin.add_maze_initialization_union(&maze_outer);
+//    dom_inner_basin.add_maze_initialization_inter(&maze_inner);
+//    dom_outer_basin.add_maze_initialization_union(&maze_outer);
 
     // ******* Algorithm ********* //
     double time_start = omp_get_wtime();
 //    omp_set_num_threads(1);
 
-    for(int i=0; i<14; i++){
+    for(int i=0; i<18; i++){
         cout << i << endl;
         subpaving.bisect();
         maze_outer.contract();
         maze_inner.contract();
 
-        maze_outer_basin.contract();
-        maze_inner_basin.contract();
+//        maze_outer_basin.contract();
+//        maze_inner_basin.contract();
     }
     cout << "TIME = " << omp_get_wtime() - time_start << endl;
 
     cout << subpaving << endl;
 
     vibes::beginDrawing();
-    VibesMaze v_maze("SmartSubPaving", &maze_outer, &maze_inner);
+    VibesMaze v_maze("SmartSubPaving",/* &maze_outer,*/ &maze_inner);
     v_maze.setProperties(0, 0, 1024, 1024);
     v_maze.show();
     v_maze.drawCircle(x1_c, x2_c, r, "r[]");
 
-    VibesMaze v_maze_basin("Basin", &maze_outer_basin, &maze_inner_basin);
-    v_maze_basin.setProperties(0, 0, 1024, 1024);
-    v_maze_basin.show();
-    v_maze_basin.drawCircle(x1_c, x2_c, r, "r[]");
+//    VibesMaze v_maze_basin("Basin", &maze_outer_basin, &maze_inner_basin);
+//    v_maze_basin.setProperties(0, 0, 1024, 1024);
+//    v_maze_basin.show();
+//    v_maze_basin.drawCircle(x1_c, x2_c, r, "r[]");
 
 //    IntervalVector position_info(2);
 //    position_info[0] = ibex::Interval(-0.5);
