44dc7cae8ce890f0115f88eed0292173755f269f
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2018-12-12 16:59:35 +0100

    [src] Add example

---
+++ examples/cpp/proof/01_van_der_pol_joris/main.cpp
@@ -0,0 +1,88 @@
+#include "ibex_SepFwdBwd.h"
+#include "smartSubPaving.h"
+#include "domain.h"
+#include "dynamicsFunction.h"
+#include "maze.h"
+#include "vibesMaze.h"
+
+#include <iostream>
+#include "vibes/vibes.h"
+#include <cstring>
+#include "graphiz_graph.h"
+#include <omp.h>
+
+using namespace std;
+using namespace ibex;
+using namespace invariant;
+
+int main(int argc, char *argv[])
+{
+    ibex::Variable x1, x2;
+
+    IntervalVector space(2);
+    space[0] = ibex::Interval(-3,3);
+    space[1] = ibex::Interval(-3,3);
+
+    // ****** Domain ******* //
+    invariant::SmartSubPaving<> subpaving(space);
+
+    invariant::Domain<> dom_outer(&subpaving, FULL_WALL);
+    dom_outer.set_border_path_in(false);
+    dom_outer.set_border_path_out(false);
+
+    ibex::Function f_joris_outer("/home/lemezoth/Documents/ensta/other/joris/constraint.txt");
+    ibex::SepFwdBwd sep_joris_outer(f_joris_outer, LEQ);
+    dom_outer.set_sep(&sep_joris_outer);
+
+    invariant::Domain<> dom_inner(&subpaving, FULL_DOOR);
+    dom_inner.set_border_path_in(true);
+    dom_inner.set_border_path_out(true);
+
+    ibex::SepNot sep_joris_inner(sep_joris_outer);
+    dom_inner.set_sep(&sep_joris_inner);
+
+    // ****** Dynamics ******* //
+    ibex::Function f(x1, x2, -Return(x2,
+                                    (1.0*(1.0-pow(x1, 2))*x2-x1)));
+    ibex::Function f2(x1, x2, -Return(x2,
+                                    (1.0*(1.0-pow(x1, 2))*x2-x1)));
+    DynamicsFunction dyn_outer(&f, FWD); // Duplicate because of simultaneous access of f (semaphore on DynamicsFunction)
+    DynamicsFunction dyn_inner(&f2, FWD);
+
+    // ******* Maze ********* //
+    invariant::Maze<> maze_outer(&dom_outer, &dyn_outer);
+    invariant::Maze<> maze_inner(&dom_inner, &dyn_inner);
+
+    // ******* Algorithm ********* //
+    double time_start = omp_get_wtime();
+//    omp_set_num_threads(1);
+
+    for(int i=0; i<15; i++){
+        cout << i << endl;
+        subpaving.bisect();
+        maze_outer.contract();
+        maze_inner.contract();
+    }
+    cout << "TIME = " << omp_get_wtime() - time_start << endl;
+
+    cout << subpaving << endl;
+
+    vibes::beginDrawing();
+    VibesMaze v_maze("SmartSubPaving", &maze_outer, &maze_inner);
+    v_maze.setProperties(0, 0, 1024, 1024);
+//    v_maze.set_enable_cone(false);
+    v_maze.show();
+
+//    IntervalVector position_info(2);
+//    position_info[0] = ibex::Interval(0);
+//    position_info[1] = ibex::Interval(1);
+//    v_maze.get_room_info(&maze, position_info);
+
+    IntervalVector position_info(2);
+    position_info[0] = ibex::Interval(-2.5);
+    position_info[1] = ibex::Interval(2.9);
+//    v_maze.show_room_info(&maze_outer, position_info);
+
+    vibes::endDrawing();
+
+}
--- src/tools/pyibex/vector_field_drawing.py
+++ src/tools/pyibex/vector_field_drawing.py
@@ -8,9 +8,11 @@ x, y = np.meshgrid(np.arange(-2.0, 2.0, 0.2),
                    np.arange(-2.0, 2.0, 0.2))
 
 u = y
-v = (8.0/25.0*x**5-4.0/3.0*x**3+4.0/5.0*x)-0.3*y
+v = ((8.0/25.0*x**5-4.0/3.0*x**3+4.0/5.0*x)-0.3*y)
 
 # v = -(x**2+y**2-1)*y-x
+
+# v = -((1-x**2)*y-x)
 
 N = np.sqrt(u**2+v**2)
 u1, v1 = u/N, v/N
@@ -18,4 +20,6 @@ u1, v1 = u/N, v/N
 # v = v/((u**2+y**2))
 
 ax.quiver(x, y, u1, v1)
+circle=plt.Circle((-0.3,0),1.0, color='r', fill=False)
+ax.add_artist(circle)
 plt.show()
