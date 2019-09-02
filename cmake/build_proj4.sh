0aef5a94021154521802dc546826801eb5346de5
Thomas Le Mézo <thomas.le_mezo@ensta-bretagne.org>
2019-09-02 14:21:57 +0200

    [src] New release

--- cmake/build_proj4.sh
+++ cmake/build_proj4.sh
@@ -11,7 +11,7 @@
 if [ ! -e "$HOME/proj4/lib/libproj.a" ]; then
 
   echo 'Installing Proj4...';
-  PROJ4_VERSION="6.0.0"
+  PROJ4_VERSION="6.2.0"
   PROJ4_FILE_NAME=proj-${PROJ4_VERSION}
   PROJ4_ARCHIVE_NAME=${PROJ4_FILE_NAME}.tar.gz
 
@@ -30,12 +30,11 @@ if [ ! -e "$HOME/proj4/lib/libproj.a" ]; then
         -DBUILD_CS2CS=OFF \
         -DBUILD_GEOD=OFF \
         -DBUILD_GIE=OFF \
-        -DBUILD_GMOCK=OFF \
         -DPROJ_TESTS=OFF \
         -DBUILD_PROJ=OFF \
         -DBUILD_PROJINFO=OFF \
          ..
+
   cmake --build . -- -j 4
-  cd ..
   make install
 fi
--- src/tools/pyibex/surface_3d.py
+++ src/tools/pyibex/surface_3d.py
@@ -6,7 +6,7 @@
 A very basic demonstration of a wireframe plot.
 '''
 
-from mpl_toolkits.mplot3d import axes3d
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
@@ -14,7 +14,7 @@ import numpy as np
 
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+ax = fig.gca(projection='3d')
 
 x, y = np.meshgrid(np.arange(-6.0, 6.0, 0.2),
                       np.arange(-6.0, 6.0, 0.2))
@@ -22,22 +22,16 @@ x, y = np.meshgrid(np.arange(-6.0, 6.0, 0.2),
 z = 2.0*np.exp((np.square(x+2)+np.square(y+2))/-10.0) + 2.0*np.exp((np.square(x-2)+np.square(y-2))/-10.0) - 10.0
 
 # # Plot a basic wireframe.
-# ax.plot_wireframe(x, y, z, rstride=1, cstride=1)
 
-# plt.show()
+surf = ax.plot_wireframe(x, y, z, rstride=1, cstride=1)
+surf.set_linewidth(0.5)
+
+cset = ax.contourf(x, y, z, zdir='z', offset=-10, cmap=cm.coolwarm)
+fig.colorbar(cset, shrink=0.5, aspect=5)
 
 
-# Plot the surface.
-surf = ax.plot_surface(x, y, z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
-
-# Customize the z axis.
-# ax.set_zlim(-1.01, 1.01)
-# ax.zaxis.set_major_locator(LinearLocator(10))
-# ax.zaxis.set_major_formatter(FormatStrFormatter('%.02f'))
-
-# Add a color bar which maps values to colors.
-fig.colorbar(surf, shrink=0.5, aspect=5)
+ax.axis('equal')
+ax.autoscale()
 
 plt.show()
-
+ 
